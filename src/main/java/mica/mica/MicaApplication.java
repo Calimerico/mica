96dddf45e034ba51dc1894f2268da60408c0d44f
spasoje <spasojepetronijevic@gmail.com>
2022-07-26 15:27:56 +0200

    Initial commit

---
+++ src/main/java/mica/mica/MicaApplication.java
@@ -0,0 +1,13 @@
+package mica.mica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicaApplication.class, args);
+	}
+
+}
